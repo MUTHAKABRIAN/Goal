33918a19df73bca9c411322353a1d1963f65dbd3
MUTHAKABRIAN <muthakabrian@gmail.com>
2020-10-02 11:00:06 +0300

    create goal property with a constructor function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  goal = 'watch Finding Nemo'
+goals:string[];
+
+constructor(){
+  this.goals = ['Watching Finding Nemo', 'Buy Cookies', 'Get new phone case']
+}
 }
