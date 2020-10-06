93b4b58beb23d794d516b03fa9cd3eb1f058081d
MUTHAKABRIAN <muthakabrian@gmail.com>
2020-10-06 10:10:04 +0300

    commit

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Goal } from '../goal';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-goal',
@@ -8,13 +9,12 @@ import { Goal } from '../goal';
 })
 export class GoalComponent implements OnInit {
 
-  goals:Goal[] = [
-    {id:1, name:'Watch finding Nemo', description:'Find an online version and watch merlin find his son'},
-    {id:2,name:'Buy Cookies', description: 'I have to buy cookies for the parrot'},
-    {id:3,name:'Get new Phone Case', description:'Diana has birthday coming up soon'},
-    {id:4,name:'Get Dog Food', description:'pupper likes expensive sancks'},
-    {id:5,name:'Solve math homework', description:'Damn math'},
-    {id:6,name:'Plot my world domination plan', description:'cause I have victory and power in Jesus name'},
+  goals:Goal[] =[
+    new Goal(1, 'Watching finding Nemo','Finding an online version and watch merlin find his son'),
+    new Goal(2, 'Get new phone case ', 'Diana has her birthday coming up soon'),
+    new Goal(3, 'Get dog food','puppy likes expensive snacks'),
+    new Goal(4, 'solve math problem', 'damn math'),
+    new Goal(5, 'plot my world domination plan','cause I am Victorious')
   ];
 
   constructor() { }
